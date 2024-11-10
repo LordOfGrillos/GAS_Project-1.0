5ade2566766fa5a6e7d016bc7a3a5c67fce54e07
Marcos Daniel Rivera Gonzalez <139647784+LordOfGrillos@users.noreply.github.com>
2024-11-10 02:52:06 -0600

    Added pointers for Ability System Component and Attributes on the character class, added replication and abilities for both enemy and character

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -2,7 +2,6 @@
 
 
 #include "Player/AuraPlayerState.h"
-
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
@@ -10,6 +9,7 @@ AAuraPlayerState::AAuraPlayerState()
 {
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 	

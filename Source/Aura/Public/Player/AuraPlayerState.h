68a2b1fb19aa94d6f8ad0999567a04bae1b94344
Marcos Daniel Rivera Gonzalez <139647784+LordOfGrillos@users.noreply.github.com>
2025-01-09 22:28:20 -0600

    Player level and XP added

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -10,6 +10,10 @@
 
 class UAbilitySystemComponent;
 class UAttributeSet;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32 /*StatValue*/);
+
+
 /**
  * 
  */
@@ -24,7 +28,17 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
+	FOnPlayerStatChanged OnXPChangedDelegate;
+	FOnPlayerStatChanged OnLevelChangedDelegate;
+
 	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+	FORCEINLINE int32 GetPlayerXP() const { return XP; }
+
+	void AddToXP(int32 InXP);
+	void AddToLevel(int32 InLevel);
+	
+	void SetXP(int32 InXP);
+	void SetLevel(int32 InLevel);
 
 protected:
 
@@ -39,6 +53,12 @@ private:
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Level)
 	int32 Level = 1;
 
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_XP)
+	int32 XP = 1;
+	
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
+
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
 };
